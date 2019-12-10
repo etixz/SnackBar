1c6d5d25c5c4c909318ab5ef52f9e22bab726b5b
etixz <egon.bomon2801@hotmail.com>
2019-12-10 16:25:30 +0100

    Added save Snack: mapping + modelattribute Added in index page: snack table (name + price)

--- src/main/java/eb/egonb/frituurvb/controller/IndexController.java
+++ src/main/java/eb/egonb/frituurvb/controller/IndexController.java
@@ -5,9 +5,12 @@ import eb.egonb.frituurvb.model.SnackDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
 
 @Controller
 public class IndexController {
@@ -20,9 +23,23 @@ public class IndexController {
         return dao.findAll();
     }
 
+    @ModelAttribute(value = "nSnack")
+    public Snack snackToSave(){
+        return new Snack();
+    }
+
     @RequestMapping(value = {"", "/", "/index"}, method = RequestMethod.GET)
     public String showIndex(ModelMap map){
         return "index";
+    }
+
+    @RequestMapping(value = {"", "/", "/index"}, method = RequestMethod.POST)
+    public String saveSnack(@Valid Snack snack, BindingResult bindingResult){
+        if(bindingResult.hasErrors()) {
+            return "index";
+        }
+        dao.save(snack);
+        return "redirect:/index";
     }
 
 }
